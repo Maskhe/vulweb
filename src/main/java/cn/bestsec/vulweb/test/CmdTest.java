6656856315945611954c497c775ced201f063307
sec <sec@hkvax-inc.com>
2026-06-29 16:45:10 +0800

    test:add cmd test

--- src/main/java/cn/bestsec/vulweb/test/CmdTest.java
+++ src/main/java/cn/bestsec/vulweb/test/CmdTest.java
@@ -5,6 +5,5 @@ import java.io.IOException;
 public class CmdTest {
     public static void main(String[] args) throws IOException {
         Runtime.getRuntime().exec(args[1]);
-        Runtime.getRuntime().exec(args[2]);
     }
 }
