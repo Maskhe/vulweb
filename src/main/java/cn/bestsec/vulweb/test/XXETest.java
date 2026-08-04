7f20ade32565c96d2e0f9e6cf5faf1c47e984e68
sec <sec@hkvax-inc.com>
2026-08-04 21:05:02 +0800

    test:fix XXETest

--- src/main/java/cn/bestsec/vulweb/test/XXETest.java
+++ src/main/java/cn/bestsec/vulweb/test/XXETest.java
@@ -2,10 +2,12 @@ package cn.bestsec.vulweb.test;
 
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.StringReader;
 
 public class XXETest {
@@ -13,7 +15,7 @@ public class XXETest {
         System.out.println("xxe test");
     }
 
-    public void test() throws ParserConfigurationException {
+    public void test() throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
 
